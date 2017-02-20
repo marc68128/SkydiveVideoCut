9bc53e46fb0189fbe64a78111fd6e25ac07b9c14
Marc <148278@supinfo.com>
2017-02-20 20:17:15 +0100

    Analyser 2

---
+++ Analyser2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Analyser2.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Analyser2
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var path = @"D:\Marc";
+            var files = Directory.EnumerateFiles(path);
+            foreach (var filePath in files)
+            {
+                var video = JsonConvert.DeserializeObject<Video>(File.ReadAllText(filePath));
+                File.Open(System.IO.Path.Combine(path, "tmp.csv"), FileMode.Create); 
+            }
+        }
+    }
+}
